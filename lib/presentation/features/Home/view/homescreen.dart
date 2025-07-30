fec65b4f7c11016654906b331771cc4ceb400c84
Mariam Mohamed <potasticart@gmail.com>
2025-07-30 18:49:17 +0300

    Update homescreen.dart

--- lib/presentation/features/Home/view/homescreen.dart
+++ lib/presentation/features/Home/view/homescreen.dart
@@ -12,7 +12,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CustomScaffold(
-      drawer: HomeDrawer(),
+      drawer: HomeDrawer(), //Signout button in Drawer 
       implyleading: true,
       body: AppCenter(children: [
           
